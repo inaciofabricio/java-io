125a11226322eb6f5ac8e2342a8a90603b6a808f
inacio.fabricio.nunes <inacio.fabricio.nunes@gmail.com>
2021-04-08 16:22:27 -0300

    Fim do 1º módulo

--- src/br/com/alura/java/io/teste/Teste.java
+++ src/br/com/alura/java/io/teste/Teste.java
@@ -1,10 +1,28 @@
 package br.com.alura.java.io.teste;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Teste {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		
+		//Fluxo de entrada de arquivo
+		FileInputStream fis = new FileInputStream("lorem.txt");
+		InputStreamReader isr = new InputStreamReader(fis);
+		BufferedReader br = new BufferedReader(isr);
+		
+		String linha = br.readLine();
+		
+		while(linha != null) {
+			
+			System.out.println(linha);
+			linha = br.readLine();
+		}
+		
+		br.close();
 	}
 
 }
