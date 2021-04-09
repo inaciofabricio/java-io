fec0964de6adb4761ac858e4e5308731b4e0e79a
inacio.fabricio.nunes <inacio.fabricio.nunes@gmail.com>
2021-04-09 13:17:16 -0300

    Fim do 4º módulo

--- src/br/com/alura/java/io/teste/TesteEscritaPrintStreamPrintWriter.java
+++ src/br/com/alura/java/io/teste/TesteEscritaPrintStreamPrintWriter.java
@@ -5,7 +5,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintStream;
 
-public class TesteEscrita3 {
+public class TesteEscritaPrintStreamPrintWriter {
 
 	public static void main(String[] args) throws IOException {
 		
---
+++ src/br/com/alura/java/io/teste/TesteLeituraCSV.java
@@ -0,0 +1,46 @@
+package br.com.alura.java.io.teste;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TesteLeituraCSV {
+
+	public static void main(String[] args) throws Exception {
+		
+		Scanner scanner = new Scanner(new File("teste.csv"));
+		
+		while(scanner.hasNext()) {
+			String linha = scanner.nextLine();
+//			System.out.println(linha);
+			
+			Scanner linhaScanner = new Scanner(linha);
+			linhaScanner.useLocale(Locale.US);
+			linhaScanner.useDelimiter(",");
+			
+			String tipo = linhaScanner.next();
+			int agencia = linhaScanner.nextInt();
+			int numero = linhaScanner.nextInt();
+			String titular = linhaScanner.next();
+			double saldo = linhaScanner.nextDouble();
+			
+			String valorFormatado = String.format(
+					new Locale("pt","BR"), 
+					"Tipo: %s, Agência: %04d, Conta: %06d, Titular: %s, Saldo: %10.2f", 
+					tipo, agencia, numero, titular, saldo
+			);
+			System.out.println(valorFormatado);
+			
+//			String[] valores = linha.split(",");
+//			System.out.println(valores[3]);
+		}
+		
+		
+	
+		
+		scanner.close();
+
+	}
+
+}
